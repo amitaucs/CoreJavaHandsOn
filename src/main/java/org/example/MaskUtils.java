3fbd4c8ebacbed27490e3b19aee58b4f256e8c5f
amitdatta <the.amisoft@gmail.com>
2023-06-18 13:30:09 +0100

    First Commit

--- src/main/java/org/example/MaskUtils.java
+++ src/main/java/org/example/MaskUtils.java
@@ -9,13 +9,15 @@ public class MaskUtils {
     public Map<String, String> maskData(Map<String, String> headersMap) {
 
         return headersMap.entrySet().stream()
-                .map(header ->
-                {
-                    if (Arrays.stream(MaskSensitiveData.values())
-                            .anyMatch(sensitiveData -> sensitiveData.name().equalsIgnoreCase(header.getValue()))) {
-                        header.setValue(MaskConstant.maskedString);
-                    }
-                    return header;
-                }).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+                .map(MaskUtils::apply)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private static Map.Entry<String, String> apply(Map.Entry<String, String> header) {
+        if (Arrays.stream(MaskSensitiveData.values())
+                .anyMatch(sensitiveData -> sensitiveData.name().equalsIgnoreCase(header.getValue()))) {
+            header.setValue(MaskConstant.maskedString);
+        }
+        return header;
     }
 }
